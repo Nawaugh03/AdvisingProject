5a119ee0290c017109d245b46de81dda42868baf
Nicholas <nickgithub03@gmail.com>
2023-03-01 14:15:54 -0500

    :)

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -42,9 +42,16 @@ class DBmanager():
         code=f"show tables;"
         cursor.execute(code)
         #print(Tablename)
-        for i in cursor.fetchone():
-            if(Tablename.lower() == i):
-                return True
+        #print(cursor.fetchone())
+        a=cursor.fetchone()
+        if (a is None):
+            cursor.close()
+            return False
+        else:
+            for i in a:
+                if(Tablename.lower() == i):
+                    cursor.close()
+                    return True
         return False
     def CreateNewDatabase(self,dbname):
         if (self.isDbExist(dbname)):
@@ -104,6 +111,8 @@ class DBmanager():
             ListofTables.append(Table(Tablename,self.GetTableContent(Tablename), self.GetTableDescription(Tablename)))
         return ListofTables
     def DisplayTables(self):
+        if (self.tables==[]):
+            print("No table listed")
         for i in self.tables:
             print(i.Name)
     def ImportToCSV(self, Table):#This function allow to import one table into a csv tile
@@ -115,95 +124,82 @@ class DBmanager():
             writer.writerow(columnnames)
             for content in (Table.Content):
                 writer.writerow(content)
-
+    
     def ExportFromCSV(self, csvname):
-        Newtable=Table(csvname)
-        Tablecolumnnames=[]
-        Tablecontent=[]
-        with open (f"{csvname}.csv",'r')as csvfile:
-            reader=csv.reader(csvfile)
-            rownumber=0
-            for row in reader:
-                if (rownumber==0):
-                    Tablecolumnnames.append(row)
-                elif(rownumber>0):
-                    Tablecontent.append(row)
-                rownumber=rownumber+1
-        Newtable.GetColumnnames(Tablecolumnnames)
-        Newtable.Getcontent(Tablecontent)
-        Newtable.GenerateDatatypes()
-        self.tables.append(Newtable)
-        #if (csvname == "list" or csvname == 'curriculum')
-        #if Table does exist Alter the Table with the content
-        #if Table doesn't exist Create a new table and insert content into the table
+        if (self.isTableExist(csvname) is False):
+            Newtable=Table(csvname)
+            Tablecolumnnames=None
+            Tablecontent=[]
+            with open(f"{csvname}.csv",'r')as csvfile:
+                reader=csv.reader(csvfile)
+                rownumber=0
+                for row in reader:
+                    if (rownumber==0):
+                        Tablecolumnnames=row
+                    elif(rownumber>0):
+                        Tablecontent.append(row)
+                        #print(Tablecontent)
+                    rownumber=rownumber+1
+            Newtable.GetColumnnames(Tablecolumnnames)
+            Newtable.Getcontent(Tablecontent)
+            Newtable.GenerateDatatypes()
+            self.tables.append(Newtable)  
+        else:
+            print("already exist")    
     def DeleteTableFromSQL(self,Table):
         #temp = mysql.connector.connect(host=self.hostname, user=self.username, password=self.password,database=self.databasename)
         cursor=self.DBconnection.cursor()
         code=f"""DROP TABLES {Table.Name};"""
         cursor.execute(code)
         self.DBconnection.commit()
-        #self.tables.remove(Table)
         cursor.close()
     def ImportTablestoSQL(self):
-        primkey = ""
-        existprime=False
         for tableindex in range(len(self.tables)):#number of tables
-            if(self.isTableExist(self.tables[tableindex].Name)== True):
+            if(self.isTableExist(self.tables[tableindex].Name)):
                 self.DeleteTableFromSQL(self.tables[tableindex])
-            else:
-                pass
-
             a = self.DBconnection.cursor()
             tablecode=f"CREATE TABLE {self.tables[tableindex].Name} (\n"
             rownum = 0
-            totalrows=len(self.tables[tableindex].columnnames[0])
+            totalrows=len(self.tables[tableindex].columnnames)
+            #print(totalrows)
+            #print(self.tables[tableindex].columntypes[0])
             #create a table
-            for index in range(len(self.tables[tableindex].columnnames[0])):#number of columns
+            for index in range(totalrows):#number of columns
                 datatype=self.tables[tableindex].columntypes[index]
-                columnname=self.tables[tableindex].columnnames[0][index]
-                notnulltext=""
-                if ("primarykey"in datatype and existprime is False):
-                    datatype = str(datatype).replace("primarykey", "")
-                    primkey= columnname
-                    existprime=True
-                    notnulltext="NOT NULL"
-                    totalrows+=1
-                tablecode+=f"{columnname} {datatype} {notnulltext}"
-                if (rownum<totalrows):
-                    tablecode+=",\n"
-                    rownum+=1
+                columnname=self.tables[tableindex].columnnames[index]
+                
+                tablecode+=f"{columnname} {datatype}"
+                if (rownum<totalrows-1):
+                    tablecode+=",\n"    
                 else:
                     tablecode+="\n"
-            if(existprime):
-                tablecode+=f"PRIMARY KEY ({primkey})\n"
-                tablecode+=");"
-            else:
-                pass
+                rownum+=1
+            tablecode+=");"
             rownum=1
-            print(tablecode)# execute statement
+            #print(tablecode)# execute statement
             a.execute(tablecode)
             self.DBconnection.commit()
             a.close()
+
             """Place the content into the table in the sql system"""
+            
+            AllColumnnames=""
+            for columnindex in range(totalrows):
+                AllColumnnames+=str(self.tables[tableindex].columnnames[columnindex])
+                if(columnindex<(totalrows-1)):
+                    AllColumnnames+=","
             Insertcode=f"INSERT INTO {self.tables[tableindex].Name} ("
-            totalrows=len(self.tables[tableindex].columnnames[0])
-            for colname in self.tables[tableindex].columnnames[0]:
-                Insertcode+=str(colname)
-                if(rownum<totalrows):
-                    Insertcode+=","
-                else:
-                    Insertcode+=")\n"
-                rownum+=1
-            totalrows=len(self.tables[tableindex].columnnames[0])
-            
-            
+            totalrows=len(self.tables[tableindex].columnnames)
+            Insertcode+=str(AllColumnnames)
+            Insertcode+=")\n"
+                
             for items in (self.tables[tableindex].Content):
                 a=self.DBconnection.cursor()
                 rownum=1
                 contentcode=Insertcode
                 contentcode+="VALUES"
                 contentcode+="("
-                for itemindex in  range(len(self.tables[tableindex].columnnames[0])):
+                for itemindex in  range(totalrows):
                     if("INT" in self.tables[tableindex].columntypes[itemindex]):
                         contentcode+=str(items[itemindex])
                     else:
@@ -214,10 +210,11 @@ class DBmanager():
                     else:
                         contentcode+=");"
                     rownum+=1
-                print(contentcode) #executable statement
+                #print(contentcode) #executable statement
                 a.execute(contentcode)
                 self.DBconnection.commit()
                 a.close()
+            
 class Table:
     def __init__(self, Tablename, tablecontent=None, tabledescription=None):
         self.Name=Tablename
@@ -233,8 +230,8 @@ class Table:
             pass
 
     def GenerateDatatypes(self):
-        primkey=False
-        for index in range(len(self.columnnames[0])):
+        #print(len(self.columnnames))
+        for index in range(len(self.columnnames)):
             columnlist=[row[index] for row in self.Content]
             #print(columnlist)
             text=""
@@ -244,9 +241,6 @@ class Table:
             elif(any(ele.isalnum() for ele in columnlist)):
                 #print("VARCHAR(255)")
                 text+="VARCHAR(255)"
-            if ("id" in str(self.columnnames[0][index]).lower() and primkey is False):
-                text+=" primarykey"
-                primkey=True
             self.columntypes.append(text)
 
     def Getcontent(self, content):
@@ -286,12 +280,22 @@ Use cases:
     A = DBmanager("localhost", "root", "1234","hawkdb")
     A.ImportToCSV(A.tables[0])
 
-
+    Use database schima
+    A = DBmanager("localhost", "root", "1234")
+    A.UseDatabase("HawkDB")
 """
 if __name__ in "__main__":
-    pass
-    #A = DBmanager("localhost", "root", "1234","hawkdb")
-    #A = DBmanager("localhost", "root", "1234")
-    #A.ExportFromCSV("Curriculums")
+    A = DBmanager("localhost", "root", "1234","hawkdb")
+    A.DisplayTables() 
+    A.ExportFromCSV("curriculums")
+    #A.tables[0].showinfo()
     #A.UseDatabase("HawkDB")
-    #A.ImportTablestoSQL()
+    A.ImportTablestoSQL()
+    
+    
+    #
+    #print(A.isTableExist(A.tables[0].Name))
+    #print(A.isTableExist("curriculums"))
+    #print(A.tables[0].columnnames)
+
+    
